537856ca081b6643045f1a160deafb39ccb795ab
Flo <fschroet@students.uni-mainz.de>
2025-07-21 00:52:39 +0200

    refactor

--- examples/example.py
+++ examples/example.py
@@ -1,3 +1,4 @@
+from optimization.local_search.meta_heuristics.hill_climbing import HillClimbing
 from optimization.local_search.meta_heuristics.simulated_annealing import (
     SimulatedAnnealing,
 )
@@ -11,17 +12,25 @@ task_durations = [1, 2, 3, 4, 4, 6, 7, 4, 4, 3, 6, 9]
 num_workers = 10
 max_load = 9
 
-instance = LocalSearchInstance(
+instance_for_simulated_annealing = LocalSearchInstance(
     task_durations, num_workers, max_load, meta_heuristic="Simulated Annealing"
 )
-initial_solution = instance.generate_feasible_solution()
-local_search = SimulatedAnnealing(instance, steps=100, temperature=1e6)
-new_sol = local_search.search(initial_solution)
+instance_for_hill_climbing = LocalSearchInstance(
+    task_durations, num_workers, max_load, meta_heuristic="Hill Climbing"
+)
+initial_solution = instance_for_simulated_annealing.generate_feasible_solution()
 
+simulated_annealing = SimulatedAnnealing(instance_for_simulated_annealing, steps=100, temperature=1e6)
+hill_climbing = HillClimbing(instance_for_simulated_annealing, steps=100)
 
-print(f"Initial solution: {initial_solution}")
-print(f"Initial score: {(init_score := instance.obj([initial_solution]))}")
+sol_simulated_annealing = simulated_annealing.search(initial_solution)
+sol_hill_climbing = hill_climbing.search(initial_solution)
+
+score_initial = instance_for_simulated_annealing.obj([initial_solution])
+score_simulated_annealing = instance_for_simulated_annealing.obj([sol_simulated_annealing])
+score_hill_climbing = instance_for_hill_climbing.obj([sol_hill_climbing])
+score_init = instance_for_simulated_annealing.obj([initial_solution])
+
+print(f"Simulated Annealing: {sol_simulated_annealing.solution}\n{score_init[0]:.2f} -> {score_simulated_annealing[0]:.2f} +{abs(score_initial[0]-score_simulated_annealing[0]):.2f}")
 print("\n")
-print(f"Final solution: {new_sol}")
-print(f"Final score: {(final_score := instance.obj([new_sol]))}")
-print(f"\nImprovement -> +{final_score[0] - init_score[0]}")
+print(f"Hill Climbing: {sol_hill_climbing.solution}\n{score_init[0]:.2f} -> {score_hill_climbing[0]:.2f}  +{abs(score_initial[0]-score_hill_climbing[0]):.2f}")
